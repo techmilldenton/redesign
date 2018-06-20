ac8e0cff105a03dc17da82067617d9211064128f
Dan Minshew <ofenixculpa@gmail.com>
2018-06-20 18:46:37 -0500

    First commit - 'frasco' base

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'extends': ['eslint:recommended', 'google'],
+  'env': {
+    // For more environments, see here: http://eslint.org/docs/user-guide/configuring.html#specifying-environments
+    'browser': true,
+    'es6': true
+  },
+  'rules': {
+    // Insert custom rules here
+    // For more rules, see here: http://eslint.org/docs/rules/
+    'no-var': 'warn',
+    'require-jsdoc': 'off'
+  }
+}
